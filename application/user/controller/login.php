6a91b395973c8075ecc6725db05938e88a5c4827
MITZ1979 <1519792930@qq.com>
2018-02-15 00:06:45 +0800

    user file cmmit

---
+++ application/user/controller/login.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/15
+ * Time: 0:02
+ */
+
+namespace app\user\controller;
+
+
+class login
+{
+    public function index($n=0 ,$m=0)
+    {
+        return '$n + $m ='. ($n + $m);
+    }
+}
