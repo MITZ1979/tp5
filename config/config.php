a78478974b0973452357395b8b19db814fde1601
MITZ1979 <1519792930@qq.com>
2018-02-16 19:17:12 +0800

    定义配置目录

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/16
+ * Time: 18:43
+ */
+/*配置修改application目录中的config.php文件*/
+return [
+    'app_trace'=>true,
+    'site_name'=>'www.nf.com',
+];
