aefbb3d498898128ec27b6346f87247dedee06d4
MITZ1979 <1519792930@qq.com>
2018-02-16 19:19:13 +0800

    定义配置扩展extra目录文件

---
+++ config/extra/my_base.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/2/16
+ * Time: 19:06
+ */
+/*扩展（extra）配置项的配置*/
+/*在tp5中返回的数都是数组*/
+return [
+    'my_name'=>'Cary',
+    'my_age'=>24,
+    'my_sex'=>'男',
+];
