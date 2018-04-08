4633512a0690dbce1d6d6f7df9605934b408d938
MITZ1979 <1519792930@qq.com>
2018-04-09 00:29:20 +0800

    commit insert

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -19,5 +19,28 @@ class Teacher extends Base
         $this->view->assign('list',$list);
         return $this->view->fetch();
     }
+    //添加教师按钮
+    public function teacherAdd()
+    {
+        $this->view->assign('添加教师');
+        return $this->view->fetch('teacher-add');
+    }
+    //添加教师的方法
+    public function AddTeacher(Request $request)
+    {
+        //从添加表中获取数据
+        $data=$request->param();
+        //更新表中的数据
+        $result=TeacherModel::create($data);
+        //
+        $status=0;
+        $message='添加失败，请检查';
+        if (true==$result) {
+            $status=1;
+            $message='添加成功';
+        }
+
+        return ['status'=>$status,'message'=>$message];
+    }
 
 }
